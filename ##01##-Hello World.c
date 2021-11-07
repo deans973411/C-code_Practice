163ad739d87055918d9e2226b9f6ac1ad0a7411d
Dean <74861204+deans973411@users.noreply.github.com>
2021-11-07 14:06:28 +0800

    Create ##01##-Hello World.c

---
+++ ##01##-Hello World.c
@@ -0,0 +1,10 @@
+/*##01##-Hello World*/
+/*Practice print Hello World*/
+#include <stdio.h>                  /*#include<stdio.h>(C), include<iostream>(C++) are preprocessing directive, put "stdio.h" file frount of code*/
+int main()                          /*int main(void) call "main() function", is a entry point and "program main body" for every C-code*/
+    {                               /*int: integer, it will return a integer to system when this C-code end execution */
+        printf("Hello World!");     /*Printf is print command in C-code, a basic function print text on monitor*/
+        return 0;                   /*"return 0" only statement in main() function. Integer 0 mean execution success no ERROR,but 1~255 express execution process somethin wrong, ERROR name defind from programer*/
+    }
+
+/*After edit, click Run > Start debugging, if it work compiler will produce a .exe file, RUN it program to check out*/
